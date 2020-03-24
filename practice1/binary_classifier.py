3e8a72990db1bfe83238f46467e9e4333077c0bd
Jihun Kim <starlettkim@gmail.com>
2020-03-24 18:28:03 +0900

    [#1] Implement except train

--- practice1/binary_classifier.py
+++ practice1/binary_classifier.py
@@ -1,5 +1,9 @@
+import math
 import random
 from typing import List, Any
+
+NUM_TRAIN = 1000
+NUM_TEST = 100
 
 
 class BinaryClassifier:
@@ -9,31 +13,56 @@ class BinaryClassifier:
 
     @staticmethod
     def generate_data(size: int):
-        ret: List[{}] = []
+        datalist: List[{}] = []
         for i in range(size):
             data = {'x1': random.randint(-10, 10), 'x2': random.randint(-10, 10)}
             if data['x1'] + data['x2'] > 0:
                 data['y'] = 1
             else:
                 data['y'] = 0
-            ret += data
-        return ret
+            datalist.append(data)
+        return datalist
 
     def train(self, datalist, learning_rate):
         pass
 
-    def predict(self, x1, x2):
-        pass
+    def __sigmoid__(self, val):
+        return 1 / (1 + math.exp(-val))
 
-    def loss(self):ㅎ
-        pass
+    def predict(self, x1, x2):
+        pred_y = self.w1 * x1 + self.w2 * x2 + self.b
+        pred_y = self.__sigmoid__(pred_y)
+        return pred_y
+
+    # Compute cross-entropy loss.
+    def loss(self, datalist):
+        y = 0
+        for data in datalist:
+            pred_y = self.predict(data['x1'], data['x2'])
+            y -= data['y'] * math.log(pred_y) + (1 - data['y']) * math.log(1 - pred_y)
+        y /= len(datalist)
+        return y
 
 
 classifier = BinaryClassifier()
+train_data = classifier.generate_data(1000)
+test_data = classifier.generate_data(100)
 
-datalist = classifier.generate_data(1000)
-
-for iteration in range(1, 101):
-    classifier.train(datalist, 1e-2)
-    print('Iteration #' + str(iteration) +
-          ': w1 = ' + str(classifier.w1) + ', w2 = ' + str(classifier.w2) + ', b = ' + str(classifier.b))
+for iteration in range(101):
+    if iteration:
+        classifier.train(train_data, 1e-2)
+    print('===== Iteration #' + str(iteration) + " =====")
+    print('w1 = ' + str(classifier.w1) + ', w2 = ' + str(classifier.w2) + ', b = ' + str(classifier.b))
+    print('train loss = ' + str(classifier.loss(train_data)))
+    print('test loss = ' + str(classifier.loss(test_data)))
+    print('train accuracy = ', end='')
+    num_correct = 0
+    for data in train_data:
+        num_correct += (round(classifier.predict(data['x1'], data['x2'])) == data['y'])
+    print(str(num_correct * 100 / len(train_data)) + '%')
+    print('test accuracy = ', end='')
+    num_correct = 0
+    for data in test_data:
+        num_correct += (round(classifier.predict(data['x1'], data['x2'])) == data['y'])
+    print(str(num_correct * 100 / len(test_data)) + '%')
+    print()
