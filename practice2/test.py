f22a81e2c8d1d22f756df290d443468b8eed4eb9
Jihun Kim <starlettkim@gmail.com>
2020-03-25 16:45:58 +0900

    [#2] Update test code

--- practice2/test.py
+++ practice2/test.py
@@ -2,13 +2,26 @@ from binary_classifier import *
 import time
 
 if __name__ == '__main__':
-    NUM_TEST = 100
+    NUM_RUN = 100
+    LEARN_RATE = 1e-6
+    print('Running tests...')
+    print('# of runs per test: %d' % NUM_RUN)
+    print('Learning rate: %e\n' % LEARN_RATE)
 
-    # Time comparison
-    time_start = time.time()
-    for test in range(NUM_TEST):
-        train_binary_classifier(1000, 100, 100, 1e-1)
-    time_elapsed = time.time() - time_start
-    print('Total time elapsed: %fs' % time_elapsed)
-    print('Average time elapsed: %fs' % (time_elapsed / NUM_TEST))
+    for m, K in [(10, 100), (100, 100), (1000, 100)]:
+        train_acc, test_acc = (0, 0)
+        time_start = time.time()
+        for test in range(NUM_RUN):
+            result = train_binary_classifier(m, 100, K, 1e-7)
+            train_acc += result['train_acc'] / NUM_RUN
+            test_acc += result['test_acc'] / NUM_RUN
+        time_elapsed = time.time() - time_start
+        print('Testing (m, K) = (%d, %d)' % (m, K))
+        print('w: ' + str(result['w'][0]))
+        print('b: ' + str(result['b']))
+        print('Train accuracy: %f%%' % train_acc)
+        print('Test accuracy: %f%%' % test_acc)
+        print('Time elapsed: %fs' % (time_elapsed / NUM_RUN))
+        print()
 
+    print('Done.')
