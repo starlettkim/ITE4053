0ac8e4329d49a74a795e71baf9924b945cd0cabf
Jihun Kim <starlettkim@gmail.com>
2020-04-16 02:01:10 +0900

    [3] Update activations

--- practice3/nn/activations.py
+++ practice3/nn/activations.py
@@ -5,14 +5,25 @@ class Activation(object):
     def __init__(self):
         pass
 
+    def forward(selfself, x: np.ndarray) \
+            -> np.ndarray:
+        pass
+
+    def backward(self, grad: np.ndarray) \
+            -> np.ndarray:
+        pass
+
 
 class Sigmoid(Activation):
     def __init__(self):
         super().__init__()
-        pass
+        self.y = np.ndarray
 
-    def forward(self, X: np.ndarray) -> np.ndarray:
-        return 1. / (1. + np.exp(-X))
+    def forward(self, x: np.ndarray) \
+            -> np.ndarray:
+        self.y = 1 / (1 + np.exp(-x))
+        return self.y
 
-    def backward(self, grad: np.ndarray) -> np.ndarray:
-        return
+    def backward(self, grad: np.ndarray) \
+            -> np.ndarray:
+        return self.y * (1 - self.y) * grad
