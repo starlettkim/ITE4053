022cff7ae8bb05b7692dfe826447414e951f9aac
Jihun Kim <starlettkim@gmail.com>
2020-04-15 22:05:16 +0900

    [3] Update model

--- practice3/nn/model.py
+++ practice3/nn/model.py
@@ -1,25 +1,39 @@
+from typing import Callable, List
+
 import numpy as np
+import nn
 
 
 class Model(object):
-    def __init__(self):
+    def __init__(self, loss: Callable[[np.ndarray, np.ndarray], float]):
+        self.loss = loss
         pass
 
     def fit(self, X: np.ndarray, y: np.ndarray):
         pass
 
-    def evaluate(self):
+    # Evaluate model on dataset.
+    def evaluate(self, X: np.ndarray, y: np.ndarray):
         pass
 
 
 class Sequential(Model):
-    def __init__(self, layers: []):
-        super().__init__()
+    def __init__(self, layers: List[nn.Layer], loss: Callable[[np.ndarray, np.ndarray], float]):
+        super().__init__(loss)
         self.layers = layers
 
-    def fit(self, X: np.ndarray, y: np.ndarray):
+    def __forward__(self, X: np.ndarray):
+        result = X
         for layer in self.layers:
-            X = layer.forward(X)
+            result = layer.forward(result)
+        return result
 
-    def evaluate(self):
-        pass
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        y_hat = self.__forward__(X)
+        l = self.loss(y_hat, y)
+        for layer in reversed(self.layers):
+            l = layer.backward(l)
+
+    def evaluate(self, X: np.ndarray, y: np.ndarray):
+        y_hat = self.__forward__(X)
+        return self.loss(y_hat, y)
