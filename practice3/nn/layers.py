8a5c78b0d8e26dc97a7e2fda63b6e2057b6f3533
Jihun Kim <starlettkim@gmail.com>
2020-04-16 02:11:34 +0900

    [3] Update layers

--- practice3/nn/layers.py
+++ practice3/nn/layers.py
@@ -1,12 +1,24 @@
+from typing import Optional
+
 import numpy as np
+from .activations import *
 
 
 class Layer(object):
-    def __init__(self, input_dim: int, output_dim: int):
+    def __init__(self,
+                 input_dim: int, output_dim: int,
+                 activation: Optional[Activation] = None,
+                 dropout_rate: Optional[int] = 0):
         self.input_dim = input_dim
         self.output_dim = output_dim
+        self.activation = activation
+        self.dropout_rate = dropout_rate
 
-    def forward(self, X: np.ndarray) -> np.ndarray:
+        self.dW, self.db = 0, 0
+        self.y = 0
+
+    def forward(self, x: np.ndarray) \
+            -> np.ndarray:
         pass
 
     def backward(self, grad: np.ndarray) -> np.ndarray:
@@ -17,16 +29,26 @@ class Layer(object):
 
 
 class Dense(Layer):
-    def __init__(self, input_dim: int, output_dim: int):
-        super().__init__(input_dim, output_dim)
+    def __init__(self,
+                 input_dim: int, output_dim: int,
+                 activation: Optional[type(Activation)] = None,
+                 dropout_rate: Optional[int] = 0):
+        super().__init__(input_dim, output_dim,
+                         activation,
+                         dropout_rate)
 
         self.weights = np.random.randn(output_dim, input_dim) * .1
         self.bias = np.random.randn(output_dim, 1) * .1
 
-    def forward(self, X: np.ndarray) -> np.ndarray:
-        result = np.dot(self.weights, X) + self.bias
-        return result
+    def forward(self, x: np.ndarray) \
+            -> np.ndarray:
+        self.y = np.dot(self.weights, x) + self.bias
+        return self.y
 
-    def backward(self, grad: np.ndarray) -> np.ndarray:
+    def backward(self, grad: np.ndarray) \
+            -> np.ndarray:
         pass
 
+    def update(self) \
+            -> None:
+        pass
