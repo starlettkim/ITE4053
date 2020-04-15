719dfd940d7d5faff2017c4022695f82f80593cc
Jihun Kim <starlettkim@gmail.com>
2020-04-16 02:11:46 +0900

    [3] Update task2

--- practice3/task2.py
+++ practice3/task2.py
@@ -1,16 +1,27 @@
+from typing import Tuple
+
 import nn
 import numpy as np
 
 
-def generate_data(low: int, high: int, shape: tuple):
+def generate_data(low: int, high: int,
+                  shape: Tuple[int, int]) \
+        -> Tuple[np.ndarray, np.ndarray]:
     X = np.random.randint(low, high, shape)
     y = (np.sum(X, 0) > 0).astype(int)
     return X, y
 
 
+def bce_loss(y_hat: np.ndarray,
+             y: np.ndarray) \
+        -> float:
+    return -np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+
+
 if __name__ == '__main__':
-    model = nn.Sequential(
-        [nn.layers.Dense(2, 1)]
+    model = nn.model.Sequential(
+        [nn.layers.Dense(2, 1, nn.activations.Sigmoid)],
+        bce_loss
     )
 
     train_X, train_y = generate_data(-2, 3, (2, 1000))
