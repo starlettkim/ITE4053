dfe282ce4554ea6b8f2b62159f9db45c541bfa83
Jihun Kim <starlettkim@gmail.com>
2020-04-19 02:13:10 +0900

    [3] Update data.py

--- practice3/data.py
+++ practice3/data.py
@@ -5,10 +5,6 @@ import numpy as np
 def generate_data(low: int, high: int,
                   shape: Tuple[int, int]) \
         -> Tuple[np.ndarray, np.ndarray]:
-    X = np.random.randint(low, high, shape)
-    y = (X[0] * X[0] > X[1]).reshape((1, shape[1])).astype(int)
-    return X, y
-
-
-train_x, train_y = generate_data(-2, 2, (2, 1000))
-test_x, test_y = generate_data(-2, 2, (2, 100))
+    x = np.random.randint(low, high, shape)
+    y = (x[0] * x[0] > x[1]).reshape((1, shape[1])).astype(int)
+    return x, y
