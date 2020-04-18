89a78d1e759d76cb05ab0e24ab02146b36931a09
Jihun Kim <starlettkim@gmail.com>
2020-04-18 23:25:29 +0900

    [3] Add metrics

---
+++ practice3/nn/metrics.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class Metric(object):
+    @classmethod
+    def compute(cls,
+                y: np.ndarray,
+                y_hat: np.ndarray) \
+            -> float:
+        pass
+
+
+class BinaryAccuracy(Metric):
+    name = 'BinaryAccuracy'
+
+    @classmethod
+    def compute(cls,
+                y: np.ndarray,
+                y_hat: np.ndarray) \
+            -> float:
+        return (y == np.round(y_hat)).mean()
+
+
+class BinaryCrossentropy(Metric):
+    name = 'BinaryClassEntropy'
+
+    @classmethod
+    def compute(cls,
+                y: np.ndarray,
+                y_hat: np.ndarray) \
+            -> float:
+        import nn.loss
+        return nn.loss.BinaryCrossentropy.compute(y, y_hat).mean()
