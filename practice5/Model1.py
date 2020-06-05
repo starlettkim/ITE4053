b325d1cb99a52856e8e7ba4b7a0e93ee58959e94
Jihun Kim <starlettkim@gmail.com>
2020-06-06 00:57:46 +0900

    [5] Impl done

--- practice5/Model1.py
+++ practice5/Model1.py
@@ -1,7 +1,10 @@
 from tensorflow.keras import Model
 from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.layers import Input, GaussianNoise, Conv2D
+from tensorflow.keras.layers \
+    import Input, Conv2D, Lambda
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
 import numpy as np
 
 
@@ -12,37 +15,43 @@ def main(train=False, test_img_path=None):
     x_test = x_test.astype('float32')
     x_train /= 255.0
     x_test /= 255.0
-    y_train, y_test = x_train, x_test
+    y_train, y_test = (x_train.copy(), x_test.copy())
+    x_train += np.random.normal(0, .1, x_train.shape)
+    x_test += np.random.normal(0, .1, x_test.shape)
 
     # Construct model
     input_tensor = Input(shape=(32, 32, 3))
-    x = GaussianNoise(0.1)(input_tensor)
+    x = Lambda(lambda input_tensor: input_tensor)(input_tensor)
     for _ in range(4):
         x = Conv2D(64, 3, padding='same', activation='relu')(x)
     x = Conv2D(3, 3, padding='same')(x)
     model = Model(input_tensor, x)
-    model.summary()
+
+    lr_schedule = PiecewiseConstantDecay(
+        [30 * 1563, 60 * 1563],
+        [1e-4, 1e-5, 5e-6]
+    )
+    model.compile(optimizer=Adam(lr_schedule), loss='mse')
 
     # Train
     if train:
-        model.compile(optimizer='Adam', loss='mse')
         model.fit(x_train, y_train, batch_size=32, epochs=100)
         model.save_weights('./checkpoints/model1')
     else:
-        pass
         model.load_weights('./checkpoints/model1')
 
     # Test
     if test_img_path is not None:
-        model.layers.pop(0)
         test_img = load_img(test_img_path)
-        test_img = img_to_array(test_img)
+        test_img = img_to_array(test_img).astype(np.float32) / 255.
+        new_img = np.zeros_like(test_img)
         for i in range(0, test_img.shape[0], 32):
             for j in range(0, test_img.shape[1], 32):
-                test_img[i:i+32, j:j+32] = model.predict(np.expand_dims(test_img[i:i+32, j:j+32], 0))
-        test_img = array_to_img(test_img)
-        test_img.save('Model1.png')
+                new_img[i:i+32, j:j+32] = model.predict(np.expand_dims(test_img[i:i+32, j:j+32], 0))
+        diff_img = array_to_img(new_img)
+        diff_img.save('Model1.png')
+        print('Done.')
 
 
 if __name__ == '__main__':
-    main(True, test_img_path='noisy.png')
+    main(True, 'noisy.png')
